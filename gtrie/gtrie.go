e19a8577e6b81e47667c9b5ee382b34beb25d5b7
neoul <neoul@ymail.com>
2020-11-25 10:14:30 +0900

    Updated GTrie to Trie

--- gtrie/gtrie.go
+++ gtrie/gtrie.go
@@ -1,35 +1,35 @@
 // Package gtrie is defined for the convenient use of the trie
 // [defined function]
-//  - func (gt *GTrie) Add(key string, value interface{})
-//  - func (gt *GTrie) Find(key string) (interface{}, bool)
-//  - func (gt *GTrie) FindLongestMatch(key string) (string, interface{}, bool)
-//  - func (gt *GTrie) HasKeysWithPrefix(key string) bool
-//  - func (gt *GTrie) Keys() []string
-//  - func (gt *GTrie) FuzzySearch(pre string) []string
-//  - func (gt *GTrie) PrefixSearch(pre string) []string
+//  - func (gt *Trie) Add(key string, value interface{})
+//  - func (gt *Trie) Find(key string) (interface{}, bool)
+//  - func (gt *Trie) FindLongestMatch(key string) (string, interface{}, bool)
+//  - func (gt *Trie) HasKeysWithPrefix(key string) bool
+//  - func (gt *Trie) Keys() []string
+//  - func (gt *Trie) FuzzySearch(pre string) []string
+//  - func (gt *Trie) PrefixSearch(pre string) []string
 package gtrie
 
 import (
 	"github.com/neoul/trie"
 )
 
-// GTrie = trie.Trie
-type GTrie trie.Trie
+// Trie = trie.Trie
+type Trie trie.Trie
 
 // New creates a new Trie with an initialized root Node.
-func New() *GTrie {
+func New() *Trie {
 	trie := trie.New()
-	return (*GTrie)(trie)
+	return (*Trie)(trie)
 }
 
 // Add adds a new key with a value
-func (gt *GTrie) Add(key string, value interface{}) {
+func (gt *Trie) Add(key string, value interface{}) {
 	t := (*trie.Trie)(gt)
 	t.Add(key, value)
 }
 
 // Find finds the value associated with the key.
-func (gt *GTrie) Find(key string) (interface{}, bool) {
+func (gt *Trie) Find(key string) (interface{}, bool) {
 	t := (*trie.Trie)(gt)
 	node, ok := t.Find(key)
 	if !ok {
@@ -40,7 +40,7 @@ func (gt *GTrie) Find(key string) (interface{}, bool) {
 
 // FindLongestMatch finds a longest matched key in the trie and
 // returns a matched key, inserted value.
-func (gt *GTrie) FindLongestMatch(key string) (string, interface{}, bool) {
+func (gt *Trie) FindLongestMatch(key string) (string, interface{}, bool) {
 	t := (*trie.Trie)(gt)
 	node, ok := t.FindLongestMatchedNode(key)
 	if !ok {
